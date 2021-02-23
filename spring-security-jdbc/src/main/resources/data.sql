91c105a872d3f4b3b37ba996442d7cb6275d849b
Bikash Kumar Sahoo <biki.sahoo123@gmail.com>
2021-02-23 17:34:29 +0530

    spring security-jdbc complete with default schema

--- spring-security-jdbc/src/main/resources/data.sql
+++ spring-security-jdbc/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username,password,enabled) values ('user','pass',true);
-INSERT INTO users (username,password,enabled) values ('admin','pass',true);
+INSERT INTO my_users (username,password,enabled) values ('user','pass',true);
+INSERT INTO my_users (username,password,enabled) values ('admin','pass',true);
 INSERT INTO authorities (username,authority) values ('user','ROLE_USER');
 INSERT INTO authorities (username,authority) values ('admin','ROLE_ADMIN');
